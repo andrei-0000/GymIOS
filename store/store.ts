41b77321800f7d252dbd2a857acc32fc875276d4
Andrei-Constantin <andrei-constantin.popa@estudiantat.upc.edu>
2024-08-01 13:39:16 +0200

    lint + format

---
+++ store/store.ts
@@ -0,0 +1,4 @@
+import { create } from "zustand";
+import { produce } from "immer";
+import { persist, createJSONStorage } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
